618ea9326ad84020956449bf4fb9b15908df5c35
Kunal-19 <kunal.019.kumar@gmail.com>
2023-03-01 00:15:24 +0530

    Q3

--- Q3/Binary_search.cpp
+++ Q3/Binary_search.cpp
@@ -1,6 +1,18 @@
 #include<stdc++.h>
 using namespace std;
 
+/*
+T.C = O(nlong(maxi - mini))
+S.C = O(1)
+*/
+
+int count(int arr[],int mid,int n){
+    int cnt = 0;
+    for(int i=0; i<n; i++){
+        if(arr[i]<=mid) cnt++;
+    }
+    return cnt;
+}
 
 int main(){
     int arr[7] = {5,2,6,1,7,3,10};
@@ -9,6 +21,24 @@ int main(){
     cout << "Enter the value of K to find Kth largest term: ";
     cin>> k;
 
+    int maxi = INT_MIN, mini = INT_MAX;
+
+    for(int i=0; i<n; i++){
+        if(mini>arr[i]) mini = arr[i];
+        if(maxi<arr[i]) maxi = arr[i];
+    }
+
+    int s = mini, e =maxi, mid = s +(e-s)/2;
+
+    while (s<e)
+    {
+        if(count(arr,mid,n) == k) break;
+        else if(count(arr,mid,n)>k) e = mid-1;
+        else s = mid+1;
+        
+        mid = s + (e-s)/2;
+    }
     
+    cout << "K-th Minimum : " << mid << endl;
     return 0;
 }
---
+++ Q3/priority_queue.cpp
@@ -0,0 +1,57 @@
+#include <stdc++.h>
+using namespace std;
+
+int findKthLargest(int nums[], int k, int n)
+{
+    priority_queue<int, vector<int>, greater<int> > q;
+
+    for (int i = 0; i < k; i++)
+    {
+        q.push(nums[i]);
+    }
+
+    for (int i = k; i < n; i++)
+    {
+        if (q.top() < nums[i])
+        {
+            q.pop();
+            q.push(nums[i]);
+        }
+    }
+
+    return q.top();
+}
+
+int findKthSmallest(int nums[], int k, int n)
+{
+    priority_queue<int> q;
+
+    for (int i = 0; i < k; i++)
+    {
+        q.push(nums[i]);
+    }
+
+    for (int i = k; i < n; i++)
+    {
+        if (q.top() > nums[i])
+        {
+            q.pop();
+            q.push(nums[i]);
+        }
+    }
+
+    return q.top();
+}
+
+int main()
+{
+    int arr[7] = {5, 2, 6, 1, 7, 3, 10};
+    int n = sizeof(arr) / sizeof(int);
+    int k;
+    cout << "Enter the value of K to find Kth largest term: ";
+    cin >> k;
+
+    cout << findKthLargest(arr, k, n) << endl;
+    cout << findKthSmallest(arr, k, n) << endl;
+    return 0;
+}
